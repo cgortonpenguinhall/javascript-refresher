9e307f9e69b702e3c3a1ef4e6266c8cab6b753d0
Craig Gorton <cgorton@penguinhall.org>
2022-03-28 13:17:19 -0400

    leaving for drama

--- refresher.js
+++ refresher.js
@@ -1,3 +1,22 @@
 console.log("Hello world...");
 
-// I am here
+let age = 13;
+console.log('In 10 years you will be ' + (age + 10));
+
+let name = 'Sue';
+if (name === 'bob') {
+  console.log('I knew it was you, ' + name + '!');
+} else {
+  console.log("Hey " + name + " you're no \"Bob.\"");
+}
+
+let iAmWearingABelt = true;
+let iAmWearingGlasses = false;
+
+if (iAmWearingABelt && iAmWearingGlasses) {
+  console.log('Probably not Mr. Gorton');
+}
+
+if (iAmWearingABelt && !iAmWearingGlasses) {
+  console.log('Probably is Mr. Gorton');
+}
